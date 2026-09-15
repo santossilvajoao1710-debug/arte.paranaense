dd2d5bf0c6c6372a8eaf4563c01bcbf2df85f154
santossilvajoao1710-debug <santos.silva.joao1710@escola.pr.gov.br>
2026-09-15 11:18:06 -0300

    kjjxj

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+const botoescurtir=document.queryselectorall("curtir");
+botoescurtir.forEach(function(botacutir){
+    let curtiu=false;
+    botaocurtir addEventlisnter("click", curtir);
+    function(curtir)
+    const contador=textcontet++
+    curtir=true
+    else{
+        con.textconte--;
+        curtiu=false;
+    }
+}
+    
